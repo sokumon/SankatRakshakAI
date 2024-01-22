a4878805802ceac5584f1cc8482a199a51d0e88e
Soham Kulkarni <sohamkulkarns9@gmail.com>
2024-01-23 01:42:55 +0530

    file upload works

--- rakshak/__init__.py
+++ rakshak/__init__.py
@@ -3,7 +3,9 @@ from flask_sock import Sock
 app = Flask(__name__)
 sock = Sock(app)
 
+app.config['UPLOAD_FOLDER'] = f"{app.root_path}/recordings/"
 
 from rakshak.routes import insert
 from rakshak.routes import createcall
 from rakshak.routes import voice
+from rakshak.routes import upload
---
+++ rakshak/routes/upload.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, session,jsonify
+from rakshak import app,sock
+import os
+import time
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        audio_file = request.files['audio']
+
+        # Generate a unique filename
+        unique_filename = f"audio_{int(time.time())}.wav"
+
+        # Save the audio file
+
+        saved_file_name = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+        audio_file.save(saved_file_name)
+
+        return jsonify({'status': 'success', 'message': 'Audio file received and saved successfully'})
+    except Exception as e:
+        print(e)
+        return jsonify({'status': 'error', 'message': str(e)})
