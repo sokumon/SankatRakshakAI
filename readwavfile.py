b3a7e1ac70774e4c0d790680177b75548360a158
Soham Kulkarni <sohamkulkarns9@gmail.com>
2024-01-22 20:13:47 +0530

    refactor

--- readwavfile.py
+++
@@ -1,9 +0,0 @@
-soundfile = open('callstart.wav','rb')
-sound = soundfile.read()
-
-# print(len(sound))
-# no_of_soundblocks = len(sound)/100
-# for i in range(0,len(no_of_soundblocks)):
-#     sound_block_data = soundfile.read()
-
-# sf.write('new_file.ogg', data, samplerate)
--- serverjoin.py
+++
@@ -1,4 +0,0 @@
-# flask app 
-
-from flask import Flask, render_template, redirect, url_for, request, session
-app = Flask(__name__)
